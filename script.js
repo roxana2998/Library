2adbfedac70c679a3df4a53ecc4081a6aa87e638
Roxana <ciobanuroxana2998@gmail.com>
2021-10-20 14:29:42 +0300

    added elements and remove button

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const inputs = [...document.querySelectorAll("input")];
 const addButton = document.querySelector("#add-button");
 const booksForm = document.querySelector(".books-form");
-const showBook = document.querySelector(".show-book");
-const cellTitle = document.querySelector(".cell-title");
+const bookInfo = document.querySelector(".book-info");
 
 let myLibrary = [];
 
@@ -14,10 +13,41 @@ function Book(title, author, numPages, isRead) {
     (this.numPages = numPages),
     (this.isRead = isRead),
     (this.provideInfo = function () {
-      return `${title} by ${author}, ${numPages} pages, ${
+      return `${title} ${author} ${numPages} ${
         this.isRead ? "read" : "not read yet"
       }`;
     });
+}
+
+function showBookInfo() {
+  const pTitle = document.createElement("p");
+  const pAuthor = document.createElement("p");
+  const pNumPages = document.createElement("p");
+  const pIsRead = document.createElement("p");
+  const removeButton = document.createElement("button");
+  removeButton.className = "remove-button";
+
+  for (element of myLibrary) {
+    pTitle.textContent = element.title;
+    bookInfo.appendChild(pTitle);
+    pAuthor.textContent = element.author;
+    bookInfo.appendChild(pAuthor);
+    pNumPages.textContent = element.numPages;
+    bookInfo.appendChild(pNumPages);
+    pIsRead.textContent = element.isRead;
+    bookInfo.appendChild(pIsRead);
+  }
+
+  removeButton.textContent = "remove";
+  bookInfo.appendChild(removeButton);
+
+  removeButton.addEventListener("click", function () {
+    pTitle.remove();
+    pAuthor.remove();
+    pNumPages.remove();
+    pIsRead.remove();
+    removeButton.remove();
+  });
 }
 
 booksForm.addEventListener("submit", function (event) {
@@ -28,8 +58,9 @@ booksForm.addEventListener("submit", function (event) {
     inputs[2].value,
     inputs[3].checked
   );
-  
+
   myLibrary.push(newBook);
+
   for (el of inputs) {
     if (el.type === "checkbox") {
       el.checked = false;
@@ -37,14 +68,9 @@ booksForm.addEventListener("submit", function (event) {
       el.value = "";
     }
   }
+
+  showBookInfo();
 });
-
-
-
-
-
-
-
 
 
 
